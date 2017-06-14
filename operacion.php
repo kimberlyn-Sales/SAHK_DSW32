ae1d4d169fa0913894bf237124d2fecbcc48896e
kimberlyn-sales <kimberlyn9607@gmail.com>
2017-06-14 15:15:36 -0500

    segundo codigo
    kimberlyn herrera

--- operacion.php
+++ operacion.php
@@ -1,5 +1,19 @@
 <?php
 $n1=(isset($_REQUEST["n1"])?$_REQUEST["n1"]:0);
 $n2=(isset($_REQUEST["n2"])?$_REQUEST["n2"]:0);
-echo $n1+$n2;
+$op=(isset($_REQUEST['operacion'])?$_REQUEST['operacion']:null);
+if ($op=='+'){
+	$res=$n1+$n2;
+} else {
+	if($op=='-'){
+		$res=$n1-$n2;
+	}elseif ($op=='*'){
+		$res=$n1*$n2;
+	}elseif ($op=='/'){
+		$res=$n1/$n2;
+	} else{
+		$res="operacion invalida";
+	}
+}
+echo $res;
 ?>
