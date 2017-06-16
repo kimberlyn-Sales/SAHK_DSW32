314c77b37e04de94614654b90b772120589d7f30
kimberlyn-Sales <kimberlyn9607@gmail.com>
2017-06-16 09:51:46 -0500

    ejercicio 1
    if variables y ciclos

---
+++ Ejercicio1.php
@@ -0,0 +1,22 @@
+<?php
+$variable;
+$variable=23;
+   echo $variable + 3;
+   echo $variable++;
+   echo $variable--;
+   echo $variable+=5;
+   echo $variable-=6;
+
+for($x=0; $x<$variable;$x++){
+	echo $x;
+	echo "<br>";
+}
+     echo "<ul>";
+while($variable<23){
+	echo "<li>";
+	echo $variable;
+	echo "</li>";
+	$variable++;
+}
+echo "</ul>";
+?>
